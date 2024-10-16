049c86b0801da26fb160495f6ceee20055371c74
mayemariso <katemariellabayno@gmail.com>
2024-10-16 08:16:06 +0800

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,20 +1,31 @@
-			var circle = document.getElementById("circle");
-			var upBtn = document.getElementById("upBtn");
-			var downBtn = document.getElementById("downBtn");
-			
-			var rotateValue = circle.style.transform;
-			var rotateSum;
-			
-			upBtn.onclick = function()
-			{
-				rotateSum = rotateValue + "rotate(-90deg)";
-				circle.style.transform = rotateSum;
-				rotateValue = rotateSum;
-			}
-			
-			downBtn.onclick = function()
-			{
-				rotateSum = rotateValue + "rotate(90deg)";
-				circle.style.transform = rotateSum;
-				rotateValue = rotateSum;
-			}
+var circle = document.getElementById("circle");
+var upBtn = document.getElementById("upBtn");
+var downBtn = document.getElementById("downBtn");
+
+var rotateValue = circle.style.transform || "rotate(0deg)";
+var rotateSum;
+
+upBtn.onclick = function() {
+    rotateSum = "rotate(" + (getCurrentRotation(circle) - 90) + "deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
+downBtn.onclick = function() {
+    rotateSum = "rotate(" + (getCurrentRotation(circle) + 90) + "deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
+function getCurrentRotation(el) {
+    var st = window.getComputedStyle(el, null);
+    var tr = st.getPropertyValue("transform");
+
+    if (tr === "none") return 0;
+
+    var values = tr.split('(')[1].split(')')[0].split(',');
+    var a = values[0];
+    var b = values[1];
+    var angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+    return angle;
+}
